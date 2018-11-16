7655bd0c3ab669c5cd0ed616ab41dbc99c5dbd99
M <M@qq.com>
2018-11-16 15:34:29 +0800

    29th commit

--- src/routes/login.js
+++ src/routes/login.js
@@ -12,9 +12,32 @@ function login(req, res, next) {
 
   console.log(captchaHasBeenRequested)
 
+  if (!captchaHasBeenRequested) {
+    res.json({
+      code: 0,
+      msg: '参数错误，请先获取验证码'
+    })
+
+    return // 组织后续代码执行（重要）
+  }
+
   const isMatched = !!cachedObj ? (captcha == cachedObj.captcha) : false
 
   console.log(isMatched)
+
+  if (!isMatched) {
+    res.json({
+      code: 400,
+      msg: '短信验证码错误，请重试!'
+    })
+
+    return
+  }
+
+  res.json({
+    code: 1,
+    msg: '登陆成功'
+  })
 }
 
 module.exports = login
